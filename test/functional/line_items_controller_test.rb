4a6ac83201e5d1e1555fb8371f9ffe8bc7d8ba89
Jonathan Dallas <github@jonathandallas.com>
2013-01-06 09:42:22 -0500

    updated functional tests for store
    copy from book didn't match the code in the book

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -27,7 +27,7 @@ class LineItemsControllerTest < ActionController::TestCase
   test "should create line_item" do
     assert_difference('LineItem.count') do
       post :create, product_id: products(:ruby).id 
-      #line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
+      # post :create, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
     end
 
     assert_redirected_to cart_path(assigns(:line_item).cart)
